b344fd7122628630df288842037bba57f4838756
dongduy956 <dongduy0612@gmail.com>
2023-08-27 17:05:22 +0700

    fix refreshtoken axios

--- src/helpers/httpRequestPrivate.ts
+++ src/helpers/httpRequestPrivate.ts
@@ -1,12 +1,36 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { Mutex } from 'async-mutex';
-import { PropsRequest } from '~/interfaces';
 import { configStorage } from '~/configs';
+import { PropsRequest } from '~/interfaces';
 import { authServices } from '~/services';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-const mutex = new Mutex();
+let isRefreshing = false; // Biến trạng thái
+let refreshSubscribers: ((newToken: string) => void)[] = []; // Mảng lưu trữ
+// Hàm gọi refresh token
+async function callRefreshToken() {
+    isRefreshing = true;
+    // Gọi API để lấy token mới
+    const session = Cookies.get(configStorage.login) ? JSON.parse(Cookies.get(configStorage.login) as string) : {};
+    const result = await authServices.refreshToken({
+        accessToken: session?.accessToken,
+        refreshToken: session?.refreshToken,
+    });
+    if (result.isSuccess) {
+        Cookies.set(configStorage.login, JSON.stringify(result.data), { expires: result.data.expiredTime });
+        // config.headers = {
+        //     ...config.headers,
+        //     authorization: `Bearer ${result.data.accessToken}`,
+        // };
+    } else if (result.statusCode === 404 || result.statusCode === 400) Cookies.remove(configStorage.login);
+    // Cập nhật token mới vào các request chờ
+    refreshSubscribers.forEach((subscriber) => {
+        subscriber(result.data.accessToken);
+    });
+
+    refreshSubscribers = [];
+    isRefreshing = false;
+}
 axios.interceptors.request.use(
     async (config: any) => {
         const session = Cookies.get(configStorage.login) ? JSON.parse(Cookies.get(configStorage.login) as string) : {};
@@ -24,31 +48,25 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
     (response) => response,
     async (error) => {
-        const config = error?.config;
-        if (error?.response?.status === 401 && !config?.sent) {
-            const release = await mutex.acquire();
-            try {
-                config.sent = true;
-                const session = Cookies.get(configStorage.login)
-                    ? JSON.parse(Cookies.get(configStorage.login) as string)
-                    : {};
-                const result = await authServices.refreshToken({
-                    accessToken: session?.accessToken,
-                    refreshToken: session?.refreshToken,
+        const originalRequest = error.config;
+
+        // Kiểm tra nếu request trả về mã lỗi 401 (token hết hạn)
+        if (error.response.status === 401 && !originalRequest._retry) {
+            if (isRefreshing) {
+                // Nếu đang refresh token, tạm dừng và chờ kết quả refresh
+                return new Promise((resolve) => {
+                    refreshSubscribers.push((newToken: string) => {
+                        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+                        resolve(axios(originalRequest));
+                    });
                 });
-                if (result.isSuccess) {
-                    Cookies.set(configStorage.login, JSON.stringify(result.data), { expires: result.data.expiredTime });
-                    config.headers = {
-                        ...config.headers,
-                        authorization: `Bearer ${result.data.accessToken}`,
-                    };
-                } else if (result.statusCode === 404 || result.statusCode === 400) Cookies.remove(configStorage.login);
-            } finally {
-                release();
             }
 
-            return axios(config);
+            originalRequest._retry = true;
+            // Gọi hàm refresh token và trả về kết quả của request ban đầu
+            return callRefreshToken().then(() => axios(originalRequest));
         }
+
         return Promise.reject(error);
     },
 );
